6656015b783548dc1f6febbe3d343e8c20777248
Austin Lauritsen <austin@veryaustin.com>
2017-02-12 17:17:29 -0600

    Remove uncompleted/unused tests in MenuButton spec

--- src/components/MenuButton.spec.js
+++ src/components/MenuButton.spec.js
@@ -3,17 +3,8 @@ import {shallow} from 'enzyme';
 import MenuButton from './MenuButton';
 
 describe('Menu Button Component', () => {
-  let wrapper;
-
-  beforeEach(() => {
-    wrapper = shallow(<MenuButton />);
-  })
-
-  it('renders the button component', () => {
-    
-  });
-
   it('renders the menu bar', () => {
+    const  wrapper = shallow(<MenuButton />);
     const menuButton = wrapper.find('.fa-bars').length;
     expect(menuButton).toBe(1);
   });
