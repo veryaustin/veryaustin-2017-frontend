832ac645f91ecdca67a5e39a1898159908d61e21
Austin Lauritsen <austin@veryaustin.com>
2017-02-21 13:02:22 -0600

    Update routes to include the new page components

--- src/routes.js
+++ src/routes.js
@@ -3,10 +3,14 @@ import {Route, IndexRoute} from 'react-router';
 import App from './components/App';
 import Home from './components/Home';
 import Work from './components/Work';
+import About from './components/About';
+import Contact from './components/Contact';
 
 export default (
   <Route path="/" component={App}>
     <IndexRoute component={Home} />
     <Route path="work" component={Work} />
+    <Route path="about" component={About} />
+    <Route path="contact" component={Contact} />
   </Route>
 );
