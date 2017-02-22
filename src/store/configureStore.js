2e214a443bfede13668a6752e417c2e67751cbff
Austin Lauritsen <austin@veryaustin.com>
2017-02-21 18:42:57 -0600

    Add redux-promise

--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -2,11 +2,11 @@ import {createStore, compose, applyMiddleware} from 'redux';
 import reduxImmutableStateInvariant from 'redux-immutable-state-invariant';
 import thunk from 'redux-thunk';
 import rootReducer from '../reducers';
+import ReduxPromise from '/redux-promise';
 
 function configureStoreProd(initialState) {
   const middlewares = [
-    // Add other middleware on this line...
-
+    ReduxPromise,
     // thunk middleware can also accept an extra argument to be passed to each thunk action
     // https://github.com/gaearon/redux-thunk#injecting-a-custom-argument
     thunk,
@@ -21,7 +21,7 @@ function configureStoreProd(initialState) {
 function configureStoreDev(initialState) {
   const middlewares = [
     // Add other middleware on this line...
-
+    ReduxPromise,
     // Redux middleware that spits an error on you when you try to mutate your state either inside a dispatch or between dispatches.
     reduxImmutableStateInvariant(),
 
