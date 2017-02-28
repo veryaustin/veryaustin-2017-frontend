2716a19c260c61486a3d9e9c360a7abe8ddd850d
Austin Lauritsen <austin@veryaustin.com>
2017-02-28 16:47:58 -0600

    Configure Contact.spec to use the redux store

--- src/containers/Contact.spec.js
+++ src/containers/Contact.spec.js
@@ -1,12 +1,19 @@
 import React from 'react';
 import {shallow} from 'enzyme';
+import {shallowToJson} from 'enzyme-to-json';
+import configureStore from 'redux-mock-store';
+import initialState from '../reducers/InitialState';
 import Contact from './Contact';
 
 describe('Contact Component', () => {
+  let middlewares, mockStore, store;
+  beforeEach(() => {
+    middlewares = [];
+    mockStore = configureStore(middlewares);
+    store = mockStore(initialState);
+  });
   it('renders the work component', () => {
-    const wrapper = shallow(<Contact />);
-    const contact = wrapper.find('#contact').length;
-    const expected = 1;
-    expect(contact).toEqual(expected);
+    const wrapper = shallow(<Contact store={store} />);
+    expect(shallowToJson(wrapper)).toMatchSnapshot();
   });
 });
--- src/reducers/initialState.js
+++ src/reducers/initialState.js
@@ -1,5 +1,6 @@
 export default {
   home: {},
   work: {},
-  about: {}
+  about: {},
+  contact: {}
 };
