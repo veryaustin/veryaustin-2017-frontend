4d11031607c718fb0ede81562c36b0e6cf50af13
Austin Lauritsen <austin@veryaustin.com>
2017-02-27 18:40:36 -0600

    Update about reducer to use initial state

--- src/reducers/aboutReducer.js
+++ src/reducers/aboutReducer.js
@@ -1,6 +1,7 @@
 import * as actions from '../constants/actionTypes';
+import initialState from './initialState';
 
-export default function(state = {}, action) {
+export default function(state = initialState.about, action) {
   switch(action.type) {
     case actions.ABOUT_FETCH:
       return Object.assign(...state, action.payload.data);
