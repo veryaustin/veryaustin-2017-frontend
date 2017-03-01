172cbf7710694bf7463d30b8d1c79b3dfe6a6c42
Austin Lauritsen <austin@veryaustin.com>
2017-02-28 18:40:01 -0600

    Create the contact action creator

--- src/constants/actionTypes.js
+++ src/constants/actionTypes.js
@@ -1,3 +1,4 @@
 export const HOME_FETCH = 'HOME_FETCH';
 export const WORK_FETCH = 'WORK_FETCH';
 export const ABOUT_FETCH = 'ABOUT_FETCH';
+export const CONTACT_FETCH = 'CONTACT_FETCH';
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -3,12 +3,14 @@ import {routerReducer} from 'react-router-redux';
 import homeReducer from './homeReducer';
 import workReducer from './workReducer';
 import aboutReducer from './aboutReducer';
+import contactReducer from './contactReducer';
 
 const rootReducer = combineReducers({
     routing: routerReducer,
     home: homeReducer,
     work: workReducer,
-    about: aboutReducer
+    about: aboutReducer,
+    contact: contactReducer
 });
 
 export default rootReducer;
