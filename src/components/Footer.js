75fa139a6f2d3e5ac16597efadee714e1442e521
Austin Lauritsen <austin@veryaustin.com>
2017-03-05 18:21:09 -0600

    Implement very basic styles using styled-components

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,12 +1,28 @@
 import React, {Component} from 'react';
+import styled from 'styled-components';
+import Wrapper from './Wrapper';
+import {Link} from 'react-router';
+
+// Main Footer Component Styles
+const MainFooter = styled.footer`
+  padding: 0 0 1em 0;
+  border-top: 1px solid #C5C5C5;
+  width: 100% ;
+  font-weight: 500;
+`;
+
+// Footer Link Component Styles 
+const FooterLink = styled(Link)`
+  color: #4A4A4A;
+`;
 
 // Footer Component
 const Footer = () => {
   return(
-   <footer>
-     <h5>&copy; {new Date().getFullYear()} Austin Lauritsen  | Built in Nashville.
-       Powered by <img src={require('../images/react_color.svg')}/> React + <img src={require('../images/redux_color.svg')}/> Redux</h5>
-    </footer>
+    <MainFooter>
+        <h5>&copy; {new Date().getFullYear()} Austin Lauritsen  | Built in Nashville.
+        Powered by <FooterLink to="https://facebook.github.io/react"><img src={require('../images/react_color.svg')}/> React</FooterLink> + <FooterLink to="http://redux.js.org/"><img src={require('../images/redux_color.svg')}/> Redux</FooterLink></h5>
+    </MainFooter>
   );
 };
 
